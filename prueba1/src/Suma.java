48223b0c41e0e034b0583380f04e573524b7aead
Mateo Rodriguez <mateo.rsantander@alumnos.upm.es>
2022-03-01 13:46:31 +0100

    clase suma

--- prueba1/src/Suma.java
+++ prueba1/src/Suma.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Suma {
+public class Suma {
+    private int n1, n2, acumulado = 0;
+
+    public int Suma2num(int n1, int n2){
+        return SumaTotal(n1+n2,acumulado);
+    }
+
+    public int SumaTotal(int num, int acumulado){
+        return acumulado += num;
+    }
 }
