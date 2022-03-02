422359304a2b3ebc4246cad5ac7989bf719fe171
Mateo Rodriguez <mateo.rsantander@alumnos.upm.es>
2022-03-02 14:57:06 +0100

    Se ha resuelto el ultimo conflicto

--- prueba1/src/prueba.java
+++ prueba1/src/prueba.java
@@ -2,7 +2,7 @@ public class prueba {
     public void prueba() {
         System.out.println("\n\n");
         System.out.println("\n\n");
-        System.out.println("Llego la guerra");
-        System.out.println("No habrá paz para los malvados... que no hacen pull antes");
+        System.out.println("Llego la paz");
+        System.out.println("Pero llego la guerra otra vez");
     }
 }
